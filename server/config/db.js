98045e96632193ae95744f1316a3f9967c5bb96a
Rapiwash <sistemarapiwash@gmail.com>
2024-08-17 17:29:58 -0500

    correcion

--- server/config/db.js
+++ server/config/db.js
@@ -3,8 +3,7 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
   try {
     await mongoose.connect(
-      "mongodb+srv://LaundrySystem:mE2tEjhiiHjzD4aW@laundry-system.kf46wwt.mongodb.net/db-omar-prueba?retryWrites=true&w=majority",
-      // "mongodb+srv://rapiwash:6FbQkhpF5m9hiUnL@sistemalavanderia.6uoe00l.mongodb.net/db-lava-ya?retryWrites=true&w=majority",
+      "mongodb+srv://rapiwash:6FbQkhpF5m9hiUnL@sistemalavanderia.6uoe00l.mongodb.net/db-lava-ya?retryWrites=true&w=majority",
       {
         useNewUrlParser: true,
         useUnifiedTopology: true,
